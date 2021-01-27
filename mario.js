02f9cfe72b12c3f18652c7e488fd2f4342e571ea
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2021-01-27 18:04:02 +0100

    render pyramid to the DOM instead of the console

--- mario.js
+++ mario.js
@@ -1,7 +1,37 @@
 const printPyramid = height => {
-    for (let i = 1; i <= height; i++) {
-        console.log(`${' '.repeat(height - i)}${'#'.repeat(i + 1)}`)
+    let draw = document.createElement('div')
+    draw.className = 'draw'
+    const brick = document.createElement('div')
+    brick.className = 'brick'
+
+    const divElem = divClass => {
+        const elem = document.createElement('div')
+        elem.className = divClass
+        return elem
     }
+
+    const fillLevel = (sNum, bNum, space, brick, elemCreate) => {
+        const level = []
+        while (sNum > 0) {
+            level.push(elemCreate(space))
+            sNum--
+        }
+        while (bNum > 0) {
+            level.push(elemCreate(brick))
+            bNum--
+        }
+        return level
+    }
+
+    for (let i = 1; i <= height; i++) {
+        const levelContent = fillLevel(height - i, i + 1, 'space', 'brick', divElem)
+        const level = document.createElement('div')
+        level.className = 'drawLevel'
+        level.append(...levelContent)
+        draw.append(level)
+    }
+    pyramid.append(draw)
+    construction.remove()
 }
 
-printPyramid(10);
+printPyramid(10)
